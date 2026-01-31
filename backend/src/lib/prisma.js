516e57abdd0104bc1dcf409f66045335be66eba8
Izan Rubio <izanrubiocerezo@gmail.com>
2026-01-31 17:22:06 +0100

    si11

--- backend/src/lib/prisma.js
+++ backend/src/lib/prisma.js
@@ -2,9 +2,15 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis;
 
+if (!process.env.DATABASE_URL) {
+    throw new Error('❌ DATABASE_URL is not defined');
+}
+
 const prisma =
     globalForPrisma.prisma ??
-    new PrismaClient({ log: ['error'] });
+    new PrismaClient({
+        log: ['error'],
+    });
 
 if (process.env.NODE_ENV !== 'production') {
     globalForPrisma.prisma = prisma;
